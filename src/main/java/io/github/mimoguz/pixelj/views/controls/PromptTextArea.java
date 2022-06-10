4f625807127346d67067b166bffd7f27f966820b
mimoguz <mimoguz@users.noreply.github.com>
2022-06-10 20:57:45 +0300

    Re-arrange

--- src/main/java/io/github/mimoguz/pixelj/views/controls/PromptTextArea.java
+++ src/main/java/io/github/mimoguz/pixelj/views/controls/PromptTextArea.java
@@ -1,9 +1,8 @@
 package io.github.mimoguz.pixelj.views.controls;
 
+import javax.swing.*;
 import java.awt.*;
 import java.util.Map;
-
-import javax.swing.JTextArea;
 
 /**
  * A text area control that can display a prompt text when it's empty and
@@ -21,19 +20,27 @@ public class PromptTextArea extends JTextArea {
         return promptColor;
     }
 
+    public void setPromptColor(final Color promptColor) {
+        this.promptColor = promptColor;
+    }
+
     public String getPromptText() {
         return promptText;
+    }
+
+    public void setPromptText(final String promptText) {
+        this.promptText = promptText;
     }
 
     @Override
     public void paintComponent(final Graphics graphics) {
         super.paintComponent(graphics);
-        if (
-            isFocusOwner() || (getText() != null && !getText().isEmpty()) || promptText == null
-                    || promptText.isEmpty() || promptText.isBlank()
-        ) {
+
+        if (isFocusOwner() || (getText() != null && !getText().isEmpty()) || promptText == null
+                || promptText.isEmpty() || promptText.isBlank()) {
             return;
         }
+
         final var g2d = (Graphics2D) graphics.create();
         if (hints != null) {
             g2d.setRenderingHints(hints);
@@ -41,19 +48,7 @@ public class PromptTextArea extends JTextArea {
             g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         }
         g2d.setColor(promptColor != null ? promptColor : Color.GRAY);
-        g2d.drawString(
-                promptText,
-                getInsets().left,
-                graphics.getFontMetrics().getMaxAscent() + getInsets().top
-        );
+        g2d.drawString(promptText, getInsets().left, graphics.getFontMetrics().getMaxAscent() + getInsets().top);
         g2d.dispose();
-    }
-
-    public void setPromptColor(final Color promptColor) {
-        this.promptColor = promptColor;
-    }
-
-    public void setPromptText(final String promptText) {
-        this.promptText = promptText;
     }
 }
