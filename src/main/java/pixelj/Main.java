234ac18afe27972de6009344ca1d794eb4655f82
mimoguz <mimoguz@users.noreply.github.com>
2022-06-15 20:19:10 +0300

    Simplified theming

--- src/main/java/pixelj/Main.java
+++ src/main/java/pixelj/Main.java
@@ -2,19 +2,22 @@ package pixelj;
 
 import javax.swing.WindowConstants;
 
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+
 import pixelj.resources.Resources;
-import pixelj.themes.PixeljDark;
-import pixelj.themes.PixeljLight;
 import pixelj.views.HomeView;
 
 public class Main {
-    private static final boolean USE_DARK_THEME = false;
+    private static final boolean USE_DARK_THEME = true;
 
     public static void main(final String[] args) {
+        FlatLaf.registerCustomDefaultsSource("pixelj.themes");
         if (USE_DARK_THEME) {
-            PixeljDark.setup();
+            FlatDarkLaf.setup();
         } else {
-            PixeljLight.setup();
+            FlatLightLaf.setup();
         }
         Resources.initialize(USE_DARK_THEME);
 
