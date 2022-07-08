2622c27e721423b03ee852b08fac334f0f9e73c7
mimoguz <mimoguz@users.noreply.github.com>
2022-07-08 09:10:07 +0300

    Split design and functionality.

---
+++ src/main/java/pixelj/views/glyphs_screen/AbstractAddDialog.java
@@ -0,0 +1,96 @@
+package pixelj.views.glyphs_screen;
+
+import java.awt.BorderLayout;
+import java.awt.Dialog;
+import java.awt.Frame;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import pixelj.models.BlockRecord;
+import pixelj.models.ScalarRecord;
+import pixelj.resources.Resources;
+import pixelj.views.controls.SearchableComboBox;
+import pixelj.views.shared.Borders;
+import pixelj.views.shared.Components;
+import pixelj.views.shared.Dimensions;
+import pixelj.views.shared.ScalarCellRenderer;
+
+/**
+ * AddDialog design. This should be extended to add functionality.
+ */
+abstract class AbstractAddDialog extends JDialog {
+    /**
+     * List of scalars.
+     */
+    protected final JList<ScalarRecord> listView = new JList<>();
+    /**
+     * Block selection.
+     */
+    protected final JComboBox<BlockRecord> filterBox = new SearchableComboBox<>();
+    /**
+     * Positive action.
+     */
+    protected final JButton addButton;
+    /**
+     * Negative action. Default.
+     */
+    protected final JButton cancelButton;
+
+    AbstractAddDialog(final Frame owner) {
+        super(owner, Resources.get().getString("addGlyphsTitle"), Dialog.ModalityType.APPLICATION_MODAL);
+
+        final var res = Resources.get();
+
+        listView.setCellRenderer(new ScalarCellRenderer());
+        
+        final var scrollPanel = new JScrollPane(listView);
+        scrollPanel.setMaximumSize(Dimensions.MAXIMUM);
+        
+        addButton = new JButton(res.getString("add"));
+        Components.setFixedSize(addButton, Dimensions.TEXT_BUTTON_SIZE);
+
+        cancelButton = new JButton(res.getString("cancel"));
+        Components.setFixedSize(cancelButton, Dimensions.TEXT_BUTTON_SIZE);
+
+        final var buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
+        buttonPanel.add(Box.createHorizontalGlue());
+        buttonPanel.add(addButton);
+        buttonPanel.add(Box.createRigidArea(Dimensions.MEDIUM_SQUARE));
+        buttonPanel.add(cancelButton);
+
+        final var content = new JPanel();
+        content.setLayout(new BorderLayout(Dimensions.MEDIUM_PADDING, Dimensions.MEDIUM_PADDING));
+
+        content.add(filterBox, BorderLayout.NORTH);
+        content.add(scrollPanel, BorderLayout.CENTER);
+        content.add(buttonPanel, BorderLayout.SOUTH);
+        content.setBorder(Borders.MEDIUM_EMPTY);
+        setContentPane(content);
+        getRootPane().setDefaultButton(cancelButton);
+
+        pack();
+        listView.requestFocusInWindow();
+        setSize(400, 600);
+        setResizable(true);
+    }
+
+    /**
+     * When overridden, super.setVisible should be called last.
+     * */
+    @Override
+    public void setVisible(final boolean visible) {
+        if (visible) {
+            setLocationRelativeTo(getOwner());
+            listView.requestFocusInWindow();
+        }
+        super.setVisible(visible);
+    }
+}
--- src/main/java/pixelj/views/glyphs_screen/AddDialog.java
+++ src/main/java/pixelj/views/glyphs_screen/AddDialog.java
@@ -1,52 +1,42 @@
 package pixelj.views.glyphs_screen;
 
-import java.awt.BorderLayout;
-import java.awt.Dialog;
 import java.awt.Frame;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Vector;
 
-import javax.swing.Box;
-import javax.swing.BoxLayout;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.DefaultListModel;
 import javax.swing.DefaultListSelectionModel;
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JList;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.ListSelectionModel;
 
 import pixelj.models.BlockRecord;
 import pixelj.models.ScalarRecord;
 import pixelj.resources.Resources;
-import pixelj.views.controls.SearchableComboBox;
-import pixelj.views.shared.Borders;
 import pixelj.views.shared.ScalarCellRenderer;
-import pixelj.views.shared.Components;
-import pixelj.views.shared.Dimensions;
 
-public class AddDialog extends JDialog {
+/**
+ * A dialog to select scalars to add. Application modal.
+ */
+public final class AddDialog extends AbstractAddDialog {
     private final DefaultListModel<ScalarRecord> listModel = new DefaultListModel<>();
     private final ArrayList<ScalarRecord> result = new ArrayList<>();
     private final ListSelectionModel selectionModel = new DefaultListSelectionModel();
 
     public AddDialog(final Frame owner) {
-        super(owner, Resources.get().getString("addGlyphsTitle"), Dialog.ModalityType.APPLICATION_MODAL);
+        super(owner);
 
+        selectionModel.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        listView.setModel(listModel);
+        listView.setSelectionModel(selectionModel);
+        listView.setCellRenderer(new ScalarCellRenderer());
+        
         final var res = Resources.get();
 
-        final JList<ScalarRecord> list = new JList<>();
-        selectionModel.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-        list.setModel(listModel);
-        list.setSelectionModel(selectionModel);
-        list.setCellRenderer(new ScalarCellRenderer());
-
-        final var scrollPanel = new JScrollPane(list);
-        scrollPanel.setMaximumSize(Dimensions.MAXIMUM);
-
-        final var filterBox = new SearchableComboBox<>(res.getBlocks().stream().skip(1).toList());
+        filterBox.setModel(
+                new DefaultComboBoxModel<>(new Vector<>(res.getBlocks().stream().skip(1).toList()))
+        );
         filterBox.setSelectedIndex(0);
         if (filterBox.getSelectedItem() instanceof final BlockRecord block) {
             listModel.addAll(res.getScalars(block.id()));
@@ -58,8 +48,6 @@ public class AddDialog extends JDialog {
             }
         });
 
-        final var addButton = new JButton(res.getString("add"));
-        Components.setFixedSize(addButton, Dimensions.TEXT_BUTTON_SIZE);
         addButton.setEnabled(false);
         addButton.addActionListener(event -> {
             fillResult();
@@ -69,43 +57,25 @@ public class AddDialog extends JDialog {
                 e -> addButton.setEnabled(selectionModel.getMinSelectionIndex() >= 0)
         );
 
-        final var cancelButton = new JButton(res.getString("cancel"));
-        Components.setFixedSize(cancelButton, Dimensions.TEXT_BUTTON_SIZE);
         cancelButton.addActionListener(event -> setVisible(false));
-
-        final var buttonPanel = new JPanel();
-        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
-        buttonPanel.add(Box.createHorizontalGlue());
-        buttonPanel.add(addButton);
-        buttonPanel.add(Box.createRigidArea(Dimensions.MEDIUM_SQUARE));
-        buttonPanel.add(cancelButton);
-
-        final var content = new JPanel();
-        content.setLayout(new BorderLayout(Dimensions.MEDIUM_PADDING, Dimensions.MEDIUM_PADDING));
-
-        content.add(filterBox, BorderLayout.NORTH);
-        content.add(scrollPanel, BorderLayout.CENTER);
-        content.add(buttonPanel, BorderLayout.SOUTH);
-        content.setBorder(Borders.MEDIUM_EMPTY);
-        setContentPane(content);
-        getRootPane().setDefaultButton(cancelButton);
-
-        pack();
-        list.requestFocusInWindow();
-        setSize(400, 600);
-        setResizable(true);
     }
 
+    /**
+     * @return List of scalars the user selected. If the user presses the cancel button to dismiss this
+     *         dialog, the result will be empty.
+     */
     public Collection<ScalarRecord> getResult() {
         return Collections.unmodifiableCollection(result);
     }
 
+    /**
+     *  @param visible Is visible. Note that since this dialog is modal, passing true here block the caller.
+     */
     @Override
     public void setVisible(final boolean visible) {
         if (visible) {
             result.clear();
             selectionModel.clearSelection();
-            setLocationRelativeTo(getOwner());
         }
         super.setVisible(visible);
     }
