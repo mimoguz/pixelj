ce72720b471a90eb8475860bc00d1b6bf78e1b97
mimoguz <mimoguz@users.noreply.github.com>
2022-09-05 09:54:10 +0300

    Remove unused imports

--- src/test/java/pixelj/services/JavaPropertiesServiceTests.java
+++ src/test/java/pixelj/services/JavaPropertiesServiceTests.java
@@ -12,8 +12,6 @@ import org.opentest4j.AssertionFailedError;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class JavaPropertiesServiceTests {
 
