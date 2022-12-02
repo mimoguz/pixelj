465151bd7db35c088254ed6a26ded71e93b900d0
mimoguz <mimoguz@users.noreply.github.com>
2022-12-02 14:29:38 +0300

    Try loading cli argument

--- src/main/java/pixelj/Main.java
+++ src/main/java/pixelj/Main.java
@@ -3,6 +3,8 @@ package pixelj;
 import java.awt.Color;
 import java.awt.image.RGBImageFilter;
 import java.io.IOException;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
 
 import javax.swing.UIManager;
 import javax.swing.WindowConstants;
@@ -13,9 +15,11 @@ import com.formdev.flatlaf.FlatLightLaf;
 
 import pixelj.resources.Resources;
 import pixelj.services.AppState;
+import pixelj.services.DBFileService;
 import pixelj.services.DefaultLogger;
 import pixelj.services.JavaPropertiesService;
 import pixelj.views.homewindow.HomeWindow;
+import pixelj.views.projectwindow.ProjectWindow;
 
 public final class Main {
 
@@ -23,27 +27,47 @@ public final class Main {
     }
 
     public static void main(final String[] args) {
+        final var appState = loadState();
+        initializeResources(appState);
+
+        // Try loading the program argument.
+        if (args.length == 1) {
+            try {
+                final var path = Path.of(args[0]);
+                final var project = new DBFileService().readFile(path);
+                final var view = new ProjectWindow(project, appState);
+                view.setVisible(true);
+                return;
+            } catch (InvalidPathException | IOException e) {
+                DefaultLogger.get().logError("Can't load the path: " + e.getLocalizedMessage());
+            }
+        }
+
+        // Else show the home window.
+        final var view = new HomeWindow(appState);
+        view.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        view.setVisible(true);
+    }
+
+    private static AppState loadState() {
         final var appState = new AppState();
         try {
             new JavaPropertiesService().set(appState);
         } catch (IOException e) {
             DefaultLogger.get().logInfo("Can't load the previous state: " + e.getLocalizedMessage());
         }
+        return appState;
+    }
 
-        Resources.initialize(appState.getColorTheme(), appState.getIconTheme());
-
+    private static void initializeResources(final AppState state) {
+        Resources.initialize(state.getColorTheme(), state.getIconTheme());
         UIManager.put("Component.grayFilter", new DisabledIconFilter(Resources.get().colors.disabledIcon()));
-
         FlatLaf.registerCustomDefaultsSource("pixelj.themes");
-        if (appState.isDarkTheme()) {
+        if (state.isDarkTheme()) {
             FlatDarkLaf.setup();
         } else {
             FlatLightLaf.setup();
         }
-
-        final var view = new HomeWindow(appState);
-        view.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        view.setVisible(true);
     }
 
     private static class DisabledIconFilter extends RGBImageFilter {
